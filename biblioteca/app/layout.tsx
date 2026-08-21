d7dd9814eb60c15c3aea6c9f5f091bc7d8adbbb1
Enzo Murno <enzo.murno14@gmail.com>
2026-08-21 19:33:10 -0300

    Update layout.tsx

--- biblioteca/app/layout.tsx
+++ biblioteca/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google"
 import "./globals.css";
-
+import { DataProvider } from "@/context/DataContext";
+ 
 const outfit = Outfit({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
@@ -22,7 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className="font-[Outfit,sans-serif] antialiased">{children}</body>
+      <body className="font-[Outfit,sans-serif] antialiased">
+        <DataProvider>
+          {children}
+        </DataProvider>
+      </body>
     </html>
   );
 }
